e80702c33c236431e8a3d46a71c4b83c565b0535
Константин Шерыхалин <konstantin-sherykhalin@yandex.ru>
2018-10-27 00:45:52 +0500

    Правки страниц

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import React, { Component } from 'react';
-import { Route, Switch} from 'react-router-dom';
+import React,{Component} from 'react';
+import {Route,Switch} from 'react-router-dom';
 
-import OnePageComponent from'./components/OnePageComponent';
-import TwoPageComponent from'./components/TwoPageComponent';
-import ThreePageComponent from'./components/ThreePageComponent';
+import Page1Component		from'./components/1';
+import Page2Component		from'./components/2';
+import Page3Component	from'./components/3';
 
 import logo from './logo.svg';
 import './App.css';
 
-var  routes = [
-  <Route key="1" exact { ...{path:'/one',  component: OnePageComponent }} />,
-  <Route key="2" exact { ...{path:'/two',  component: TwoPageComponent }} />,
-  <Route key="3" exact { ...{path:'/three',  component: ThreePageComponent }} />,
+var routes = [
+	<Route key="1" exact {...{path:'/one',	component: Page1Component}} />,
+	<Route key="2" exact {...{path:'/two',	component: Page2Component}} />,
+	<Route key="3" exact {...{path:'/three',component: Page3Component}} />,
 ];
 
-export default () => <Switch>{routes}</Switch>
-
-
-
+export default () => (<Switch>{routes}</Switch>);
