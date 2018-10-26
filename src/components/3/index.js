7769a2b25de19e039a6cdd56825bd2dcc25fb529
Константин Шерыхалин <konstantin-sherykhalin@yandex.ru>
2018-10-27 00:53:17 +0500

    Четвертая страница для Аарифа

--- src/components/3/index.js
+++ src/components/3/index.js
@@ -3,13 +3,7 @@ import React,{Component} from 'react';
 export default class App extends Component {
 	render() {
 		return (
-			<div className="">
-				<h1>Начни составлять свой бизнес-план сейчас</h1>
-				<label className="">Автор идеи</label>
-				<input className="" alt="logo" />
-				<label className="">Инвестор</label>
-				<input className="" alt="logo" />
-			</div>
+			<div className="">Страница 4</div>
 		);
 	}
 }
