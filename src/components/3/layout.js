242ae956c4c414d44f053c73f7f5e8b2677fc3ec
Константин Шерыхалин <konstantin-sherykhalin@yandex.ru>
2018-10-27 02:22:37 +0500

    Влив из css

--- src/components/3/layout.js
+++ src/components/3/layout.js
@@ -3,10 +3,9 @@ import React from 'react';
 export default () =>  (
 	<div className="container">
           <div className="first-q">
-              <h2>Давай познакомимся</h2>
-              <p>loreAdipisicing cupidatat ut officia aute proident mollit sunt consequat eiusmod excepteur ut sit velit aute consectetur ex aliquiproident amet duis fugiat et quis amet sint dolore amet exercitation veniam dolore veniam anim duis.</p>
+              <h2>Одно правило, </h2>
               <h3>Какая у тебя идея?</h3>
-              <textarea rows="10"></textarea>
+              <p>loreAdipisicing cupidatat ut officia aute proident mollit sunt consequat eiusmod excepteur ut sit velit aute consectetur ex aliquiproident amet duis fugiat et quis amet sint dolore amet exercitation veniam dolore veniam anim duis.</p>
               <input type="submit" className="button" value="Готово" />
           </div>
     </div>
