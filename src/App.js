fd514baaf0814e9580d8b66bd93fa1c54f23f9a6
lenar15 <boy.ap@yandex.ru>
2018-10-27 00:31:58 +0500

    lenar-commit-end

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React, { Component } from 'react';
+import { Route, Switch} from 'react-router-dom';
+
+import OnePageComponent from'./components/OnePageComponent';
+import TwoPageComponent from'./components/TwoPageComponent';
+import ThreePageComponent from'./components/ThreePageComponent';
+
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+var  routes = [
+  <Route key="1" exact { ...{path:'/one',  component: OnePageComponent }} />,
+  <Route key="2" exact { ...{path:'/two',  component: TwoPageComponent }} />,
+  <Route key="3" exact { ...{path:'/three',  component: ThreePageComponent }} />,
+];
 
-export default App;
+export default () => <Switch>{routes}</Switch>
+
+
+
---
+++ src/components/ThreePageComponent/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="">
+          <h1>ggНачни составлять свой бизнес-план сейчас</h1>
+          <label className="">Автор идеи</label>
+          <input className="" alt="logo" />
+          <label className="">Инвестор</label>
+          <input className="" alt="logo" />
+      </div>
+    );
+  }
+}
