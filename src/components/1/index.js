72b8a951ff9190134f685833ee6ea7b270225e9d
lenar15 <boy.ap@yandex.ru>
2018-10-27 00:51:37 +0500

    lenar-commit-end

--- src/components/1/index.js
+++ src/components/1/index.js
@@ -1,27 +1,28 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+
 
 export default class App extends Component {
-	render() {
-		return (
-			<div className="">
-				<div className="">
-					<h1>Начни составлять свой бизнес-план сейчас</h1>
-					<label className="">Автор идеи</label>
-					<input className="" alt="logo" />
-					<label className="">Инвестор</label>
-					<input className="" alt="logo" />
-				</div>
-				<div className="">
-					<h1>С чем связана твоя идея?</h1>
-					<select>
-						<option>IT-стартап</option>
-						<option>Салон красоты</option>
-						<option>Пекарня</option>
-						<option>Интернет-магазин</option>
-						<option>Рекламное агенство</option>
-					</select>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="first-q">
+          <div className="">
+              <h1>Начни составлять свой бизнес-план сейчас</h1>
+              <input id="one" type="radio" />
+              <label for="one">Автор идеи</label>
+              <input id="two" type="radio" />
+              <label for="two">Инвестор</label>
+          </div>
+          <div className="second-q">
+              <h1>С чем связана твоя идея?</h1>
+              <select>
+                <option>IT-стартап</option>
+                <option>Салон красоты</option>
+                <option>Пекарня</option>
+                <option>Интернет-магазин</option>
+                <option>Рекламное агенство</option>
+              </select>
+          </div>
+      </div>
+    );
+  }
 }
