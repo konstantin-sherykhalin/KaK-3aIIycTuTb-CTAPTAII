daff720a3916938b772c70238d4d021602987edd
lenar15 <boy.ap@yandex.ru>
2018-10-27 11:16:42 +0500

    lenar

--- src/components/4/layout.js
+++ src/components/4/layout.js
@@ -2,6 +2,31 @@ import React from 'react';
 
 export default (props) =>  (
 	<div style={{display:'flex',flexDirection:'row'}}>
-		<div style={{width:'25%'}}>{props.state.step.map((e,i) => <div key={i}>{e.name}</div>)}</div>
+		<div className="li-container">
+			<ul>
+				<li>Проверка идеи ></li>
+				<li>Построение гипотезы ></li>
+				<li>Проверка гипотезы ></li>
+				<li>Составление roadmap ></li>
+			</ul>
+		</div>
+		<div>
+			<h1>Проверка идеи</h1>
+			<h2>Что такое бизнес-канвас?</h2>
+			<p>Dolore commodo excepteur dolore dolor sit exercitation non officia
+			 laboris sed eiusmod esse ullamco ullamco fugiat id ad 
+			  qui excepteur anim exercitation mollit esse tempor est ut 
+			  proident proident minim tempor tempor deserunt com
+			  modo qui deserunt incididunt dolore cupidatat sunt dolore
+			   consequat fugiat ea dolor aliqua culpa duis cupidatat vo
+			   luptate incididunt ullamco incididunt adipisicing sed te
+			   mpor sed tempor exercitation consectetur ut ut adipisicing
+			    id commodo ullamco anim nisi exercitation ut ex cillum vol
+			</p>
+		</div>
+		<div>
+			<span>Стоимость идеи</span>
+			<span>1$</span>
+		</div>
 	</div>
 );
