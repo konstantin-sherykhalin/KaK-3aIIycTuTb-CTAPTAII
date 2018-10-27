5ec8f57acb27a65b960469019cc3d8dfec68bb97
Константин Шерыхалин <konstantin-sherykhalin@yandex.ru>
2018-10-27 12:50:06 +0500

    Ответы

--- src/components/4/index.js
+++ src/components/4/index.js
@@ -7,27 +7,51 @@ export default class App extends Component {
 		step: [
 			{
 				name: 'Проблема и решение',
+				title: 'Что такое бизнес канвас?',
+				text: 'Это какая-то дичь.',
+				test: false,
 				question: 'Вы решили запустить сервис онлайн бухгалтерии. Укажите, что относится к преимуществам',
 				answers: ['Электронный документооборот','Дешево','Удаленный доступ','Автоматизация учета'],
 				answer: -1,
 				true_answer: 2,
+				canvas: false,
 			},
 			{
 				name: 'Рынок и конкуренты',
-				active: false,
-				available: false,
 			},
 			{
 				name: 'Проверка гипотезы',
-				active: false,
-				available: false,
+			},
+			{
+				name: 'Показатели успеха',
+			},
+			{
+				name: 'Юнит-экономика',
+			},
+			{
+				name: 'Роадмап',
 			},
 		],
+		active: 0,
+		available: 0,
 	};
+
+	start_test = () => {
+		let step = this.state.step;
+		step[this.state.active].test = true;
+		this.setState({step});
+		console.log(this.state);
+	}
 
 	set_answer = (answer) => {
 		let step = this.state.step;
 		step[this.state.active].answer = answer;
+		this.setState({step});
+	}
+
+	start_canvas = () => {
+		let step = this.state.step;
+		step[this.state.active].canvas = true;
 		this.setState({step});
 	}
 
@@ -38,6 +62,13 @@ export default class App extends Component {
 	}
 
 	render() {
-		return (<Layout state={this.state} />);
+		return (
+			<Layout
+				state={this.state}
+				start_test={this.start_test}
+				set_answer={this.set_answer}
+				next_step={this.next_step}
+			/>
+		);
 	}
 }
--- src/components/4/layout.js
+++ src/components/4/layout.js
@@ -1,36 +1,43 @@
 import React from 'react';
 
-export default (props) =>  (
-	<div className="var-container" style={{display:'flex',flexDirection:'row'}}>
-		<div className="left-container">
-			<ul>
-				<li>Проверка идеи ></li>
-				<li>Построение гипотезы ></li>
-				<li>Проверка гипотезы ></li>
-				<li>Показатели успеха ></li>
-				<li>Юнит-экономика</li>
-				<li>Роадмап</li>
-			</ul>
+export default (props) => {
+	let data = props.state.step[props.state.active];
+	return (
+		<div className="var-container" style={{display:'flex',flexDirection:'row'}}>
+			<div className="left-container">
+				<ul>
+				{props.state.step.map((e,i) => (
+					<li key={i}>{e.name} ></li>
+				))}
+				</ul>
+			</div>
+			<div className="middle-container">
+				<h1>{data.name}</h1>
+				{!data.test ? (
+					<div>
+						<h2>{data.title}</h2>
+						<p>{data.text}</p>
+						<input type="submit" className="button" value="Я готов пройти тест" onClick={props.start_test} />
+					</div>
+				) : (
+					(data.answer === -1 || data.answer !== data.true_answer) ? (
+						<div>
+							<h2>{data.question}</h2>
+							{data.answers.map((e,i) => (<p key={i} onClick={_ => props.set_answer(i)}>{e}</p>))}
+							{(data.answer !== -1 && data.answer !== data.true_answer) && (<p>Неправильный ответ!</p>)}
+						</div>
+					) : (
+						<div>
+							<p>Правильный ответ!</p>
+							<input type="submit" className="button" value="Заполнить канвас доску" onClick={props.start_canvas} />
+						</div>
+					)
+				)}
+			</div>
+			<div className="right-container">
+				<div>Стоимость идеи:</div>
+				<div>1$</div>
+			</div>
 		</div>
-		<div className="middle-container">
-			<h1>Проверка идеи</h1>
-			<h2>Что такое бизнес-канвас?</h2>
-			<p>Dolore commodo excepteur dolore dolor sit exercitation non officia
-			 laboris sed eiusmod esse ullamco ullamco fugiat id ad 
-			  qui excepteur anim exercitation mollit esse tempor est ut 
-			  proident proident minim tempor tempor deserunt com
-			  modo qui deserunt incididunt dolore cupidatat sunt dolore
-			   consequat fugiat ea dolor aliqua culpa duis cupidatat vo
-			   luptate incididunt ullamco incididunt adipisicing sed te
-			   mpor sed tempor exercitation consectetur ut ut adipisicing
-			    id commodo ullamco anim nisi exercitation ut ex cillum vol
-			</p>
-			<div>Лекция о бизнес-канвас</div>
-			<input type="submit" className="button" value="Я готов пройти тест" />
-		</div>
-		<div className="right-container">
-			<div>Стоимость идеи:</div>
-			<div>1$</div>
-		</div>
-	</div>
-);
+	);
+}
